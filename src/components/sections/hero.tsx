007f0b7a761e1162f814b94f5524c0a22556ec80
igorfc211-source <igorfc211@gmail.com>
2026-06-02 14:03:45 -0300

    refactor

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import {
   ArrowRight,
   ChevronDown,
