2c219c5d00db53c3ac0c42e2fd8529ba75ea2bbf
igorfc211-source <igorfc211@gmail.com>
2026-06-03 14:11:14 -0300

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 
-import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
